f406356239a7d9c9d0cba3f482c0500ea72dc223
Elizabeth-E <elizabeth.erickson21@gmail.com>
2021-05-18 23:37:17 +0200

    Database seeding
    added accounts to database through seeding. also added iban model. will later be added to a service.

--- src/main/java/io/swagger/service/AccountService.java
+++ src/main/java/io/swagger/service/AccountService.java
@@ -7,5 +7,6 @@ import java.util.List;
 public interface AccountService
 {
     Account addAccount(Account account);
+    List<Account>  addAccount(List<Account> accountList);
     List<Account> getAccounts();
 }
--- src/main/java/io/swagger/service/AccountServiceImpl.java
+++ src/main/java/io/swagger/service/AccountServiceImpl.java
@@ -9,16 +9,35 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AccountServiceImpl implements AccountService{
+// TODO: This is breaking the application
+/*
 
+ */
+public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
-
+    /**
+     * Add an account to the database.
+     *
+     * @param account Account model to store
+     * @return Account model that was stored
+     */
     @Override
     public Account addAccount(Account account) {
         accountRepository.save(account);
         return account;
+    }
+
+    /**
+     * Add a list of account to the database
+     *
+     * @param accountList List of Account models to store
+     * @return List Account models that were stored
+     */
+    public List<Account> addAccount(List<Account> accountList) {
+        accountRepository.saveAll(accountList);
+        return accountList;
     }
 
     @Override
