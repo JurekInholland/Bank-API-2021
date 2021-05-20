14ced6c247ab25767ce9cd94ff72f4228f4d6f77
Mish97 <mihaela.yordanova8@gmail.com>
2021-05-20 20:21:10 +0200

    Implemented Transaction Service

---
+++ src/main/java/io/swagger/service/TransactionServiceImpl.java
@@ -0,0 +1,44 @@
+package io.swagger.service;
+
+import io.swagger.model.Account;
+import io.swagger.model.Transaction;
+import io.swagger.model.User;
+import io.swagger.repository.TransactionRepository;
+import io.swagger.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Override
+    public Transaction addTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public Transaction executeTransaction(Account fromAccount, Account toAccount, BigDecimal amount) {
+        return null;
+    }
+
+    @Override
+    public Transaction getTransactionById(int id) {
+        return transactionRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Override
+    public void deleteTransactionById(int id) {
+        transactionRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Transaction> getTransactions() {
+        return (List<Transaction>) transactionRepository.findAll();
+    }
+}
