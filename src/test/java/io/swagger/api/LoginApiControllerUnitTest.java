03e9ae2d7a8307bb7368b619c480125be2fed683
Mish97 <mihaela.yordanova8@gmail.com>
2021-05-23 00:45:16 +0200

    Started Unit Tests Setup

---
+++ src/test/java/io/swagger/api/LoginApiControllerUnitTest.java
@@ -0,0 +1,12 @@
+package io.swagger.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginApiControllerUnitTest {
+
+    @Test
+    void loginUser() {
+    }
+}
---
+++ src/test/java/io/swagger/api/UsersApiControllerUnitTest.java
@@ -0,0 +1,28 @@
+package io.swagger.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersApiControllerUnitTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
