419bff21bdef9e7fb88bddb7ba04f26c1edc27fd
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-05-22 18:30:09 +0200

    CreateUserDTO project lombok
    Implemented project Lombok into CreateUserDTO and removed redundant code. Also added @Nonnull annotations in the constructor

--- src/main/java/io/swagger/model/CreateUserDto.java
+++ src/main/java/io/swagger/model/CreateUserDto.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.model.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
+
+import lombok.Data;
+import lombok.NonNull;
 import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.*;
@@ -18,221 +21,24 @@ import javax.validation.constraints.*;
 @Validated
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-21T13:43:31.154Z[GMT]")
-
-
-
+@Data
 public class CreateUserDto   {
-  @JsonProperty("id")
+
   @Id
-  private BigDecimal id = null;
+  private BigDecimal id;
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+  private String emailAddress;
+  private String password;
+  private Role role;
 
-  @JsonProperty("firstName")
-  private String firstName = null;
-
-  @JsonProperty("lastName")
-  private String lastName = null;
-
-  @JsonProperty("phoneNumber")
-  private String phoneNumber = null;
-
-  @JsonProperty("emailAddress")
-  private String emailAddress = null;
-
-  @JsonProperty("password")
-  private String password = null;
-
-  @JsonProperty("role")
-  private Role role = null;
-
-  public CreateUserDto id(BigDecimal id) {
-    this.id = id;
-    return this;
-  }
-
-  /**
-   * Get id
-   * @return id
-   **/
-  @Schema(description = "")
-  
-    @Valid
-    public BigDecimal getId() {
-    return id;
-  }
-
-  public void setId(BigDecimal id) {
-    this.id = id;
-  }
-
-  public CreateUserDto firstName(String firstName) {
+  public CreateUserDto(@NonNull String firstName, @NonNull String lastName, @NonNull String phoneNumber, @NonNull String emailAddress, @NonNull String password, @NonNull Role role) {
     this.firstName = firstName;
-    return this;
-  }
-
-  /**
-   * Get firstName
-   * @return firstName
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public CreateUserDto lastName(String lastName) {
     this.lastName = lastName;
-    return this;
-  }
-
-  /**
-   * Get lastName
-   * @return lastName
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public CreateUserDto phoneNumber(String phoneNumber) {
     this.phoneNumber = phoneNumber;
-    return this;
-  }
-
-  /**
-   * Get phoneNumber
-   * @return phoneNumber
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    public String getPhoneNumber() {
-    return phoneNumber;
-  }
-
-  public void setPhoneNumber(String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
-
-  public CreateUserDto emailAddress(String emailAddress) {
     this.emailAddress = emailAddress;
-    return this;
-  }
-
-  /**
-   * Get emailAddress
-   * @return emailAddress
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    public String getEmailAddress() {
-    return emailAddress;
-  }
-
-  public void setEmailAddress(String emailAddress) {
-    this.emailAddress = emailAddress;
-  }
-
-  public CreateUserDto password(String password) {
     this.password = password;
-    return this;
-  }
-
-  /**
-   * Get password
-   * @return password
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public CreateUserDto role(Role role) {
     this.role = role;
-    return this;
-  }
-
-  /**
-   * Get role
-   * @return role
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    @Valid
-    public Role getRole() {
-    return role;
-  }
-
-  public void setRole(Role role) {
-    this.role = role;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    CreateUserDto createUserDto = (CreateUserDto) o;
-    return Objects.equals(this.id, createUserDto.id) &&
-        Objects.equals(this.firstName, createUserDto.firstName) &&
-        Objects.equals(this.lastName, createUserDto.lastName) &&
-        Objects.equals(this.phoneNumber, createUserDto.phoneNumber) &&
-        Objects.equals(this.emailAddress, createUserDto.emailAddress) &&
-        Objects.equals(this.password, createUserDto.password) &&
-        Objects.equals(this.role, createUserDto.role);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, firstName, lastName, phoneNumber, emailAddress, password, role);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class CreateUserDto {\n");
-    
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
-    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
-    sb.append("    phoneNumber: ").append(toIndentedString(phoneNumber)).append("\n");
-    sb.append("    emailAddress: ").append(toIndentedString(emailAddress)).append("\n");
-    sb.append("    password: ").append(toIndentedString(password)).append("\n");
-    sb.append("    role: ").append(toIndentedString(role)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
   }
 }
--- src/main/java/io/swagger/model/User.java
+++ src/main/java/io/swagger/model/User.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.validation.annotation.Validated;
 import javax.persistence.*;
 import javax.validation.Valid;
