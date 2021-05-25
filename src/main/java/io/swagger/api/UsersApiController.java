5f81f34d1c3f2a7e97785f17ce0f7484216fda26
Mish97 <mihaela.yordanova8@gmail.com>
2021-05-25 18:54:38 +0200

    Make GET /users return PublicUsers

--- src/main/java/io/swagger/api/UsersApiController.java
+++ src/main/java/io/swagger/api/UsersApiController.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -61,11 +62,13 @@ public class UsersApiController implements UsersApi {
         this.request = request;
     }
 
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<Void> createUser(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody CreateUserDto body) {
         userService.addUser(convertToEntity(body));
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('EMPLOYEE')")
     public ResponseEntity<Void> deleteUser(@Parameter(in = ParameterIn.PATH, description = "The userid for the user to delete", required=true, schema=@Schema()) @PathVariable("userid") Integer userid) {
         userService.deleteUserById(userid);
         return new ResponseEntity<Void>(HttpStatus.OK);
@@ -76,10 +79,17 @@ public class UsersApiController implements UsersApi {
         User user = userService.getUserById(userid);
         return new ResponseEntity<>(user, HttpStatus.OK);    }
 
-    public ResponseEntity<List<User>> getUsers(@Parameter(in = ParameterIn.QUERY, description = "The number of items to skip before starting to collect the result set" ,schema=@Schema()) @Valid @RequestParam(value = "offset", required = false, defaultValue="0") Integer offset,@Parameter(in = ParameterIn.QUERY, description = "The numbers of items to return" ,schema=@Schema()) @Valid @RequestParam(value = "limit", required = false, defaultValue = "100") Integer limit) {
+    public ResponseEntity<List<PublicUserDto>> getUsers(@Parameter(in = ParameterIn.QUERY, description = "The number of items to skip before starting to collect the result set" ,schema=@Schema()) @Valid @RequestParam(value = "offset", required = false, defaultValue="0") Integer offset,@Parameter(in = ParameterIn.QUERY, description = "The numbers of items to return" ,schema=@Schema()) @Valid @RequestParam(value = "limit", required = false, defaultValue = "100") Integer limit) {
 
         List<User> users = userService.getUsers();
-        return new ResponseEntity<>(users.subList(Math.min(users.size(), offset),Math.min(users.size(), offset + limit)),HttpStatus.OK);
+        List<PublicUserDto> publicUsers = new ArrayList<>();
+
+
+        users.forEach(user -> {
+            publicUsers.add(convertToPublicDto(user));
+        });
+
+        return new ResponseEntity<>(publicUsers.subList(Math.min(users.size(), offset),Math.min(users.size(), offset + limit)),HttpStatus.OK);
     }
 
     public ResponseEntity<Void> updateUser(@Parameter(in = ParameterIn.PATH, description = "The userid for the user to update", required=true, schema=@Schema()) @PathVariable("userid") Integer userid,@Parameter(in = ParameterIn.DEFAULT, description = "", schema=@Schema()) @Valid @RequestBody ModifyUserDto body) {
@@ -100,9 +110,9 @@ public class UsersApiController implements UsersApi {
     }
 //    Convert User to PublicUserDto
 //
-//    private PublicUserDto convertToPublicDto(User user) {
-//        PublicUserDto publicUser = modelMapper.map(user, PublicUserDto.class);
-//        return publicUser;
-//    }
+    private PublicUserDto convertToPublicDto(User user) {
+        PublicUserDto publicUser = modelMapper.map(user, PublicUserDto.class);
+        return publicUser;
+    }
 
 }
