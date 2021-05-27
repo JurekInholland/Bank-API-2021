46ecc1d5515f4d5cd456f366f8891aff7b8f3c73
Jurek Baumann <jurekbaumann@me.com>
2021-05-27 04:21:03 +0200

    Slightly ugly fix to updateUser

--- src/main/java/io/swagger/service/UserServiceImpl.java
+++ src/main/java/io/swagger/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package io.swagger.service;
 
+import io.swagger.api.exception.InvalidRequestException;
 import io.swagger.api.exception.UserNotFoundException;
 import io.swagger.model.ModifyUserDto;
 import io.swagger.model.User;
@@ -7,9 +8,11 @@ import io.swagger.repository.AccountRepository;
 import io.swagger.repository.TransactionRepository;
 import io.swagger.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.ToLongFunction;
 
 @Service
@@ -22,6 +25,9 @@ public class UserServiceImpl implements UserService
 
     @Autowired
     private TransactionRepository transactionRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
 
     public User addUser(User user)
     {
@@ -38,9 +44,33 @@ public class UserServiceImpl implements UserService
     }
     public void updateUser(ModifyUserDto modifyUserDto, long id)
     {
-        userRepository.updateUserById(
-                modifyUserDto.getFirstName(), modifyUserDto.getLastName(), modifyUserDto.getPhoneNumber(),
-                modifyUserDto.getEmailAddress(), modifyUserDto.getPassword(), modifyUserDto.getRoles(),id);
+
+        User u = userRepository.findById(id).orElseThrow(() -> new InvalidRequestException("User not found."));
+
+        if (modifyUserDto.getFirstName() != null) {
+            u.setFirstName(modifyUserDto.getFirstName());
+        }
+        if (modifyUserDto.getLastName() != null) {
+            u.setLastName(modifyUserDto.getLastName());
+
+        }
+        if (modifyUserDto.getEmailAddress() != null) {
+            u.setEmailAddress(modifyUserDto.getEmailAddress());
+        }
+        if (modifyUserDto.getRoles() != null) {
+            u.setRoles(modifyUserDto.getRoles());
+        }
+        if (modifyUserDto.getPassword() != null) {
+            u.setPassword(encoder.encode(modifyUserDto.getPassword()));
+        }
+        if (modifyUserDto.getPhoneNumber() != null) {
+            u.setPassword(modifyUserDto.getPhoneNumber());
+        }
+
+        userRepository.save(u);
+//        userRepository.updateUserById(
+//                modifyUserDto.getFirstName(), modifyUserDto.getLastName(), modifyUserDto.getPhoneNumber(),
+//                modifyUserDto.getEmailAddress(), modifyUserDto.getPassword(), modifyUserDto.getRoles(),id);
     }
     public void deleteUserById(long id)
     {
