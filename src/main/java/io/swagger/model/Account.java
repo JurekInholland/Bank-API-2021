22f5490e060627ad40a71ed4648dc63e7e6df950
Elizabeth-E <elizabeth.erickson21@gmail.com>
2021-05-27 23:15:00 +0200

    lombok refactoring

--- src/main/java/io/swagger/model/Account.java
+++ src/main/java/io/swagger/model/Account.java
@@ -7,6 +7,8 @@ import io.swagger.model.AccountType;
 import io.swagger.model.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
+
+import lombok.*;
 import org.springframework.validation.annotation.Validated;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -17,36 +19,35 @@ import javax.validation.constraints.*;
 /**
  * Account
  */
+@Data
 @Validated
 @Entity
+@NoArgsConstructor
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-20T14:30:32.476Z[GMT]")
 
 
 public class Account   {
+  @Schema(example = "NLxxINHO0xxxxxxxxx", required = true, description = "")@NotNull
   @Id
   @JsonProperty("iban")
   private String iban = null;
 
+  @Schema(required = true, description = "")@NotNull@Valid
   @OneToOne
   @JsonProperty("user")
   private User user = null;
 
+  @Schema(required = true, description = "")@NotNull@Valid
   @JsonProperty("balance")
   private BigDecimal balance = null;
 
+  @Schema(required = true, description = "")@NotNull@Valid
   @JsonProperty("accountType")
   private AccountType accountType = null;
 
+  @Schema(description = "Balance cannot become lower than a certain number defined per account, referred to as absolute limit")@Valid
   @JsonProperty("absoluteLimit")
   private BigDecimal absoluteLimit = new BigDecimal(0);
-
-  public Account iban(String iban) {
-    this.iban = iban;
-    return this;
-  }
-  // TODO: Make Model cleaner
-  public Account () {
-  }
 
   public Account ( String iban, User user, BigDecimal balance, AccountType accountType){
     this.iban = iban;
@@ -55,145 +56,5 @@ public class Account   {
     this.accountType = accountType;
 
   }
-  /**
-   * Get iban
-   * @return iban
-   **/
-  @Schema(example = "NLxxINHO0xxxxxxxxx", required = true, description = "")
-      @NotNull
 
-    public String getIban() {
-    return iban;
-  }
-
-  public void setIban(String iban) {
-    this.iban = iban;
-  }
-
-  public Account user(User user) {
-    this.user = user;
-    return this;
-  }
-
-  /**
-   * Get user
-   * @return user
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    @Valid
-    public User getUser() {
-    return user;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
-  }
-
-  public Account balance(BigDecimal balance) {
-    this.balance = balance;
-    return this;
-  }
-
-  /**
-   * Get balance
-   * @return balance
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    @Valid
-    public BigDecimal getBalance() {
-    return balance;
-  }
-
-  public void setBalance(BigDecimal balance) {
-    this.balance = balance;
-  }
-
-  public Account accountType(AccountType accountType) {
-    this.accountType = accountType;
-    return this;
-  }
-
-  /**
-   * Get accountType
-   * @return accountType
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    @Valid
-    public AccountType getAccountType() {
-    return accountType;
-  }
-
-  public void setAccountType(AccountType accountType) {
-    this.accountType = accountType;
-  }
-
-
-
-  /**
-   * Balance cannot become lower than a certain number defined per account, referred to as absolute limit
-   * @return absoluteLimit
-   **/
-  @Schema(description = "Balance cannot become lower than a certain number defined per account, referred to as absolute limit")
-
-  @Valid
-  public BigDecimal getAbsoluteLimit() {
-    return absoluteLimit;
-  }
-
-  public void setAbsoluteLimit(BigDecimal absoluteLimit) {
-    this.absoluteLimit = absoluteLimit;
-  }
-
-
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Account account = (Account) o;
-    return Objects.equals(this.iban, account.iban) &&
-        Objects.equals(this.user, account.user) &&
-        Objects.equals(this.balance, account.balance) &&
-        Objects.equals(this.accountType, account.accountType);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(iban, user, balance, accountType);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Account {\n");
-    
-    sb.append("    iban: ").append(toIndentedString(iban)).append("\n");
-    sb.append("    user: ").append(toIndentedString(user)).append("\n");
-    sb.append("    balance: ").append(toIndentedString(balance)).append("\n");
-    sb.append("    accountType: ").append(toIndentedString(accountType)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
--- src/main/java/io/swagger/model/CreateAccountDto.java
+++ src/main/java/io/swagger/model/CreateAccountDto.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.model.AccountType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -13,6 +14,7 @@ import javax.validation.constraints.*;
 /**
  * CreateAccountDto
  */
+@Data
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-21T13:43:31.154Z[GMT]")
 
@@ -20,132 +22,20 @@ import javax.validation.constraints.*;
 public class CreateAccountDto {
 
 
+  @Schema(description = "")
   @JsonProperty("iban")
   private String iban = null;
 
+  @Schema(required = true, description = "")@NotNull
   @JsonProperty("userId")
   private Long userId = null;
 
+  @Schema(required = true, description = "")@NotNull
   @JsonProperty("balance")
   private BigDecimal balance = null;
 
+  @Schema(required = true, description = "")@NotNull@Valid
   @JsonProperty("accountType")
   private AccountType accountType = null;
 
-  public CreateAccountDto iban(String iban) {
-    this.iban = iban;
-    return this;
-  }
-
-  /**
-   * Get iban
-   * @return iban
-   **/
-  @Schema(description = "")
-  
-    public String getIban() {
-    return iban;
-  }
-
-  public void setIban(String iban) {
-    this.iban = iban;
-  }
-
-  public CreateAccountDto userId(Long userId) {
-    this.userId = userId;
-    return this;
-  }
-
-  /**
-   * Get userId
-   * @return userId
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    public Long getUserId() {
-    return userId;
-  }
-
-  public void setUserId(Long userId) {
-    this.userId = userId;
-  }
-
-  public CreateAccountDto accountType(AccountType accountType) {
-    this.accountType = accountType;
-    return this;
-  }
-
-  /**
-   * Get accountType
-   * @return accountType
-   **/
-  @Schema(required = true, description = "")
-      @NotNull
-
-    @Valid
-    public AccountType getAccountType() {
-    return accountType;
-  }
-
-  public void setAccountType(AccountType accountType) {
-    this.accountType = accountType;
-  }
-
-  /**
-   * Get balance
-   * @return balance
-   **/
-  @Schema(required = true, description = "")
-  @NotNull
-
-  public BigDecimal getBalance() {
-    return balance;
-  }
-
-  public void setBalance(BigDecimal balance) {
-    this.balance = balance;
-  }
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    CreateAccountDto createAccountDto = (CreateAccountDto) o;
-    return Objects.equals(this.iban, createAccountDto.iban) &&
-        Objects.equals(this.userId, createAccountDto.userId) &&
-        Objects.equals(this.accountType, createAccountDto.accountType);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(iban, userId, accountType);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class CreateAccountDto {\n");
-    
-    sb.append("    iban: ").append(toIndentedString(iban)).append("\n");
-    sb.append("    userId: ").append(toIndentedString(userId)).append("\n");
-    sb.append("    accountType: ").append(toIndentedString(accountType)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 }
