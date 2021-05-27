b64a7f55b9da16cbc851be6799959d5761ad35b7
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-05-27 17:38:14 +0200

    Fixed Update User and Create User
    Fixed the update user issue. You can now update a user only if the fields are not null nor empty. Same case with Create User.

--- src/main/java/io/swagger/model/User.java
+++ src/main/java/io/swagger/model/User.java
@@ -1,21 +1,10 @@
 package io.swagger.model;
-
 import java.math.BigDecimal;
 import java.util.Collection;
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import io.swagger.model.Role;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.springframework.validation.annotation.Validated;
 import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-21T13:43:31.154Z[GMT]")
 @Entity
--- src/main/java/io/swagger/service/UserServiceImpl.java
+++ src/main/java/io/swagger/service/UserServiceImpl.java
@@ -1,16 +1,16 @@
 package io.swagger.service;
 
 import io.swagger.api.exception.UserNotFoundException;
+import io.swagger.model.CreateUserDto;
 import io.swagger.model.ModifyUserDto;
 import io.swagger.model.User;
 import io.swagger.repository.AccountRepository;
 import io.swagger.repository.TransactionRepository;
 import io.swagger.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.function.ToLongFunction;
 
 @Service
 public class UserServiceImpl implements UserService
@@ -23,10 +23,13 @@ public class UserServiceImpl implements UserService
     @Autowired
     private TransactionRepository transactionRepository;
 
+    @Autowired
+    PasswordEncoder encoder;
+
     public User addUser(User user)
     {
-        userRepository.save(user);
-        return user;
+       userRepository.save(user);
+       return user;
     }
     public User getUserById(long id)
     {
@@ -38,9 +41,28 @@ public class UserServiceImpl implements UserService
     }
     public void updateUser(ModifyUserDto modifyUserDto, long id)
     {
-        userRepository.updateUserById(
-                modifyUserDto.getFirstName(), modifyUserDto.getLastName(), modifyUserDto.getPhoneNumber(),
-                modifyUserDto.getEmailAddress(), modifyUserDto.getPassword(), modifyUserDto.getRoles(),id);
+        User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+
+        if (modifyUserDto.getFirstName() != null && !modifyUserDto.getFirstName().isEmpty()) {
+            user.setFirstName(modifyUserDto.getFirstName());
+        }
+        if (modifyUserDto.getLastName() != null && !modifyUserDto.getLastName().isEmpty()) {
+            user.setLastName(modifyUserDto.getLastName());
+        }
+        if (modifyUserDto.getEmailAddress() != null && !modifyUserDto.getEmailAddress().isEmpty()) {
+            user.setEmailAddress(modifyUserDto.getEmailAddress());
+        }
+        if (modifyUserDto.getRoles() != null && !modifyUserDto.getRoles().isEmpty()) {
+            user.setRoles(modifyUserDto.getRoles());
+        }
+        if (modifyUserDto.getPassword() != null && !modifyUserDto.getPassword().isEmpty()) {
+            user.setPassword(encoder.encode(modifyUserDto.getPassword()));
+        }
+        if (modifyUserDto.getPhoneNumber() != null && !modifyUserDto.getPhoneNumber().isEmpty()) {
+            user.setPassword(modifyUserDto.getPhoneNumber());
+        }
+
+        userRepository.save(user);
     }
     public void deleteUserById(long id)
     {
